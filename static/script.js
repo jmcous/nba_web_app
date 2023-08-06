74ff56b77e7251cb8c56cdb3939ebcce6a8672b0
Justin Cousineau <jmcous@gmail.com>
2023-08-06 15:37:34 -0500

    made script.js more modular

--- static/script.js
+++ static/script.js
@@ -112,80 +112,110 @@ $(document).ready(function() {
 		return plotdata;
 	  }
 	  
-
-	function plotGraph(data){
-
-
-		//check if there is a statz that can be used
-		// if no, then plot 2d scat chart
+	function plotGraph(data) {
+		let traces = [];
+		let layout = {};
+		const gridcolor = 'rgba(255, 255, 255,0.3)';
+		const config = { responsive: true };
+	
 		if (data.z.constructor == String) {
-			console.log(data.color);
-			// if linear regression button is checked, plot line
-			if ($('#linreg:checked').val() == 1){
-
-
-
-
-				var trace1 = {
-					x: data.x,
-					y: data.y,
-					mode: 'markers',
-					type: 'scatter',
-					text: data.lineups,
-					//marker:{color: '#43FF33'},
-					marker:{color: data.color},
-					name: 'Lineups'
-				};
-
-				var trace2 = {
-					x: data.x,
-					y: data.y_pred,
-					mode: 'lines',
-					marker:{color: '#43FF33'},
-					name: 'Linear Regression'
-				};
-
-
-				var traces = [trace1, trace2];
-				
-				var plotdata = plotSelectedTeam(data, traces,'xy');
-
-				
-				const squared = "2"
-				var rsq_string = "R"+squared.sup()+": "+data.pred_results[0];
-				var int_string = "Intercept: "+data.pred_results[1];
-				var coef_string = "Coefficient: "+data.pred_results[2];
-				
-				const rsq_container = document.getElementById('rsq_container');
-				const int_container = document.getElementById('int_container');
-				const coef1_container = document.getElementById('coef1_container');
-
-				rsq_container.innerHTML = rsq_string;
-				//int_container.innerHTML = int_string;
-				//coef1_container.innerHTML = coef_string;
-
-				
-			} else { // if it is not selected then just plot the scatters
-				var trace1 = {
+			traces = plot2DGraph(data);
+			layout = get2DLayout($('#statx').val(), $('#staty').val(), gridcolor);
+		} else {
+			traces = plot3DGraph(data);
+			layout = get3DLayout($('#statx').val(), $('#staty').val(), $('#statz').val(), gridcolor);
+		}
+	
+		Plotly.newPlot('plot', traces, layout, config);
+	}
+	
+	function plot2DGraph(data) {
+		let traces = [];
+		if ($('#linreg:checked').val() == 1) {
+			traces.push({
 				x: data.x,
 				y: data.y,
 				mode: 'markers',
 				type: 'scatter',
 				text: data.lineups,
-				marker:{color: data.color},
+				marker: { color: data.color },
 				name: 'Lineups'
-				};
-				
-				var traces = [trace1];
-				var plotdata = plotSelectedTeam(data, traces,'xy');
-
-				
-				var lr_string = "";
-				const rsq_container = document.getElementById('rsq_container');
-				rsq_container.innerHTML = lr_string;
-			}
-			var gridcolor = 'rgba(255, 255, 255,0.3)'
-			var layout = {
+			}, {
+				x: data.x,
+				y: data.y_pred,
+				mode: 'lines',
+				marker: { color: '#43FF33' },
+				name: 'Linear Regression'
+			});
+			updateRegressionResults(data);
+		} else {
+			traces.push({
+				x: data.x,
+				y: data.y,
+				mode: 'markers',
+				type: 'scatter',
+				text: data.lineups,
+				marker: { color: data.color },
+				name: 'Lineups'
+			});
+			clearRegressionResults();
+		}
+		return plotSelectedTeam(data, traces, 'xy');
+	}
+	
+	function plot3DGraph(data) {
+		let traces = [];
+		if ($('#linreg:checked').val() == 1) {
+			traces.push({
+				x: data.x,
+				y: data.y,
+				z: data.z,
+				mode: 'markers',
+				type: 'scatter3d',
+				text: data.lineups,
+				marker: { color: data.color, size: 5 },
+				name: 'Lineups'
+			}, {
+				x: data.x_pred,
+				y: data.y_pred,
+				z: data.z_pred,
+				type: 'mesh3d',
+				colorscale: 'rgb(255, 0, 255)',
+				opacity: 0.5,
+				name: 'Linear Regression'
+			});
+			updateRegressionResults(data);
+		} else {
+			traces.push({
+				x: data.x,
+				y: data.y,
+				z: data.z,
+				mode: 'markers',
+				type: 'scatter3d',
+				text: data.lineups,
+				marker: { color: data.color, size: 5 },
+				name: 'Lineups'
+			});
+			clearRegressionResults();
+		}
+		return plotSelectedTeam(data, traces, 'xyz');
+	}
+	
+	function updateRegressionResults(data) {
+		const squared = "2";
+		const rsq_container = document.getElementById('rsq_container');
+		rsq_container.innerHTML = "R" + squared.sup() + ": " + data.pred_results[0];
+		// Additional code for other regression results can be added here
+	}
+	
+	function clearRegressionResults() {
+		const rsq_container = document.getElementById('rsq_container');
+		rsq_container.innerHTML = "";
+		// Additional code to clear other regression results can be added here
+	}
+	
+	function get2DLayout(statx, staty, gridcolor) {
+		return {
 				height: 600,
 				xaxis: {
 					title:{
@@ -233,171 +263,90 @@ $(document).ready(function() {
 				showlegend: false
 
 				
-			};
-			
-			var config = {responsive: true};
-
-			
-			
-			Plotly.newPlot('plot', plotdata, layout,config);
-			
-		} else {//plot 3d scatter plot
-
-		
-			// if linear regression button is checked, get different traces
-			if ($('#linreg:checked').val() == 1){
-			
-				var trace1 = {
-					x: data.x,
-					y: data.y,
-					z: data.z,
-					mode: 'markers',
-					type: 'scatter3d',
-					text: data.lineups,
-					marker:{color: data.color,size:5},
-					name: 'Lineups'
-
-				};
-
-				var trace2 = {
-					x: data.x_pred,
-					y: data.y_pred,
-					z: data.z_pred,
-					type: 'mesh3d',
-					colorscale: 'rgb(255, 0, 255)',
-					opacity: 0.5,
-					name: 'Linear Regression'
-				};
-				
-				var traces = [trace1, trace2];
-				var plotdata = plotSelectedTeam(data, traces,'xyz');
-
-				
-				const squared = "2"
-				var rsq_string = "R"+squared.sup()+": "+data.pred_results[0];
-				var int_string = "Intercept: "+data.pred_results[1];
-				var coef1_string = "Coefficients: "+data.pred_results[2][0]+",";
-				var coef2_string = data.pred_results[2][1];
-				
-				const rsq_container = document.getElementById('rsq_container');
-				const int_container = document.getElementById('int_container');
-				const coef1_container = document.getElementById('coef1_container');
-				const coef2_container = document.getElementById('coef2_container');
-
-
-				rsq_container.innerHTML = rsq_string;
-				//int_container.innerHTML = int_string;
-				//coef1_container.innerHTML = coef1_string;
-				//coef2_container.innerHTML = coef2_string;
-
-				
-				
-			} else {
-			
-				var trace1 = {
-					x: data.x,
-					y: data.y,
-					z: data.z,
-					mode: 'markers',
-					type: 'scatter3d',
-					text: data.lineups,
-					marker:{color: data.color,size:5},
-					name: 'Lineups'
-				};
-
-				var traces = [trace1];
-				var plotdata = plotSelectedTeam(data, traces,'xyz');
-				
-				var lr_string = "";
-				const rsq_container = document.getElementById('rsq_container');
-				rsq_container.innerHTML = lr_string;
-			}
-			
-			var layout = {
-				height: 800,
-				scene: {
-					xaxis: {
-						title:{
-							text: $('#statx').val(),
-							color: 'white',
-							font: {
-								family: 'Courier New',
-								size: 18,
-							}
-						},
-						tickfont: {
-							color: 'white',
-							family: 'Courier New',
-							size: 15
-						},
-						showline: true,
-						linecolor: 'white',
-						gridcolor: gridcolor,
-						linewidth: 1,
-						color: 'white'
-					},
-					yaxis: {
-						title: {
-							text: $('#staty').val(),
-							color: 'white',
-							font: {
-								family: 'Courier New',
-								size: 18,
-							}
-						},
-						tickfont: {
-							color: 'white',
-							family: 'Courier New',
-							size: 15
-						},
-						showline: true,
-						gridcolor: gridcolor,
-						linewidth: 1,
-						color: 'white'
-					},
-					zaxis: {
-						title: {
-							text: $('#statz').val(),
-							color: 'white',
-							font: {
-								family: 'Courier New',
-								size: 18,
-							}
-						},
-						tickfont: {
-							color: 'white',
-							family: 'Courier New',
-							size: 15
-						},
-						showline: true,
-						gridcolor: gridcolor,
-						linewidth: 1,
-						color: 'white'
-					},
-					showlegend: false
-				},
-					margin: {
-					l: 10,
-					r: 10,
-					b: 10,
-					t: 10,
-					pad: 4
-				},
-				plot_bgcolor: 'black',
-				paper_bgcolor: 'black',
-			};
-			
-			var config = {responsive: true};
-			
-			Plotly.newPlot('plot', plotdata, layout,config);				
-			
-		
-		}
-
-
-
-
-
+		};
 	}
+	
+	function get3DLayout(statx, staty, statz, gridcolor) {
+		return {
+			height: 800,
+			scene: {
+				xaxis: {
+					title:{
+						text: $('#statx').val(),
+						color: 'white',
+						font: {
+							family: 'Courier New',
+							size: 18,
+						}
+					},
+					tickfont: {
+						color: 'white',
+						family: 'Courier New',
+						size: 15
+					},
+					showline: true,
+					linecolor: 'white',
+					gridcolor: gridcolor,
+					linewidth: 1,
+					color: 'white'
+				},
+				yaxis: {
+					title: {
+						text: $('#staty').val(),
+						color: 'white',
+						font: {
+							family: 'Courier New',
+							size: 18,
+						}
+					},
+					tickfont: {
+						color: 'white',
+						family: 'Courier New',
+						size: 15
+					},
+					showline: true,
+					gridcolor: gridcolor,
+					linewidth: 1,
+					color: 'white'
+				},
+				zaxis: {
+					title: {
+						text: $('#statz').val(),
+						color: 'white',
+						font: {
+							family: 'Courier New',
+							size: 18,
+						}
+					},
+					tickfont: {
+						color: 'white',
+						family: 'Courier New',
+						size: 15
+					},
+					showline: true,
+					gridcolor: gridcolor,
+					linewidth: 1,
+					color: 'white'
+				},
+				showlegend: false
+			},
+				margin: {
+				l: 10,
+				r: 10,
+				b: 10,
+				t: 10,
+				pad: 4
+			},
+			plot_bgcolor: 'black',
+			paper_bgcolor: 'black',
+		};
+	}
+	
+
+
+	
+
+
+
 
 });
